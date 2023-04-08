03cb28acc19088dd789f7acedfc7951e4dd8386c
Rhonald Mendoza <rhonald78@gmail.com>
2023-04-07 20:04:44 -0400

    arreglo en la indentacion

--- factorial.js
+++ factorial.js
@@ -1,13 +1,13 @@
 function factorial(num) {
-	if ((num==0) || (num==1)) { 
-		return 1
-	} else {
-		let total = 1
-		for (i=1; i<=num; i++) {
-			total = total * i;
-		}
-		return total;
-	}
+  if (num == 0 || num == 1) {
+    return 1;
+  } else {
+    let total = 1;
+    for (i = 1; i <= num; i++) {
+      total = total * i;
+    }
+    return total;
+  }
 }
 
-module.exports = { factorial }
+module.exports = { factorial };
